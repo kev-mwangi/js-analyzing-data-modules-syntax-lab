b4ff6cdbae26bf57f21c46880f39b3f957d6a067
Kelvin <kevinmwas007@gmail.com>
2026-04-15 12:36:42 +0300

    add data

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+const datejs = require("datejs");
 
+function combineUsers(...args) {
 
+   
+    const combinedObject = {
+        users: []
+    };
+  for (let arr of args) {
+        combinedObject.users = [
+            ...combinedObject.users,
+            ...arr
+        ];
+    }
+combinedObject.merge_date = new Date().toString("M/d/yyyy");
+     return combinedObject;
+}
+console.log(
+    combineUsers(
+        ["Kelvin", "Eric"],
+        ["Mwangi", "Chege"],
+        ["Maina"]
+    )
+);
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
